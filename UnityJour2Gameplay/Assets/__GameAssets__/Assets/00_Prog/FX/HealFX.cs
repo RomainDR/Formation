8107ec3f0cc2965677b84759db8a34f5e48d1f0b
RomainDR <romain.drouhot@orange.fr>
2026-04-15 17:36:47 +0200

    add FX

---
+++ UnityJour2Gameplay/Assets/__GameAssets__/Assets/00_Prog/FX/HealFX.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class HealFX : MonoBehaviour
+{
+        private Transform _target;
+        private VisualEffect _healFx;
+
+        private void Awake()
+        {
+                _healFx = GetComponent<VisualEffect>();
+                _target = GameManager.Instance.GetPlayer().transform;
+                
+                Destroy(gameObject, 5.0f);
+        }
+
+        private void SetTarget(Transform target)
+        {
+                _target = target;
+        }
+
+        private void Update()
+        {
+                _healFx.SetVector3("Direction Particle",  Vector3.Normalize(_target.position + new Vector3(0f,0.5f,0f) - transform.position) / 2);
+        }
+}
--- UnityJour2Gameplay/Assets/__GameAssets__/Assets/00_Prog/GPE/Boxs/HealBox/HealBox.cs
+++ UnityJour2Gameplay/Assets/__GameAssets__/Assets/00_Prog/GPE/Boxs/HealBox/HealBox.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.VFX;
 
 public class HealBox : Collectible
 {
     [SerializeField] private int lifeToGain = 1;
-
+    [SerializeField] private HealFX healVFX;
+    
     public void AddLife(Player player)
     {
         player.AddLife(lifeToGain);
         Debug.Log("Add life to player");
+        
+        Instantiate(healVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
